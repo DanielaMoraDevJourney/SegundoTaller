e52d9bb9d6d02b09736898e2f6277e3a98de3a33
laboratorio <laboratorio@UPOAULA10322>
2024-04-10 10:53:40 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DanielaMora_PrimerTaller.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
